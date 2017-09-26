87551d1f737fe392d6455d1b1cf224b98d3d6f24
Cedric Remond <cedric.remond@fabriquartz.com>
2017-09-26 11:31:35 +0200

    initial commit

---
+++ lib/hardsteen_v2.ex
@@ -0,0 +1,18 @@
+defmodule HardsteenV2 do
+  @moduledoc """
+  Documentation for HardsteenV2.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HardsteenV2.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hardsteen_v2_test.exs
@@ -0,0 +1,8 @@
+defmodule HardsteenV2Test do
+  use ExUnit.Case
+  doctest HardsteenV2
+
+  test "greets the world" do
+    assert HardsteenV2.hello() == :world
+  end
+end
